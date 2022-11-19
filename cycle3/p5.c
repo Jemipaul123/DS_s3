f3a9276820f803cc6c64439faec50e8898762813
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2022-11-19 10:14:17 +0530

    Update p5.c

--- cycle3/p5.c
+++ cycle3/p5.c
@@ -1,33 +1,26 @@
-//POSTFIX EVALUATION
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 #define MAX 100
 
-int stack[MAX], top = -1; // stack array and top
+int stack[MAX], top = -1; 
 
-// Push function to insert element
+
 void push(int data)
 {
     if (top == MAX - 1)
-    {
-        printf("\nStack overflow\n Aborting\n");
-        exit(0);
-    }
+        printf("\nStack overflow");
     else
         stack[++top] = data;
 }
 
-// Pop function to pop element
+
 int pop()
 {
     int del;
     if (top == -1)
     {
-        printf("\nStack empty\n Invalid Expression\n");
-        exit(0);
+        printf("\nStack empty");
+        return -1;
     }
     else
     {
@@ -37,7 +30,7 @@ int pop()
 }
 
 
-// Displays top element without popping it
+
 int peek()
 {
 	if(top == -1)
@@ -46,8 +39,8 @@ int peek()
 		return stack[top];
 }
 
-// debug
-// To print elements of stack
+
+
 void display()
 {
     if (top == -1)
@@ -62,58 +55,79 @@ void display()
     }
 }
 
-// A utility function to check if
-// the given character is operand
-int isOperand(char ch)
+void convertToBinary(int num)
 {
-    return  (isdigit(ch));
+    while (num)
+    {
+        push(num%2);
+        num /= 2;
+    }
 }
 
-int evalPostfix( char exp[] )
+void displayBinary()
 {
-    for(int i=0; i<strlen(exp); i++)
+    printf("\nBinary : ");
+    while(top != -1)
     {
-        if(isOperand(exp[i]))
-        {
-            // convert char to appropriate single-digit
-            push(exp[i]-'0');
-        }
-        else
-        {
-            int op2 = pop();
-            int op1 = pop();
-            int result;
-            switch(exp[i])
-            {
-                case '+':
-                    result = op1+op2;
-                    break;
-                case '-':
-                    result = op1-op2;
-                    break;
-                case '*':
-                    result = op1*op2;
-                    break;
-                case '/':
-                    result = op1/op2;
-                    break;
-                default:
-                    printf("Unknown operator\n");
-                    exit(0);
-            }
-            push(result);
-        }
+        printf("%d",pop());
     }
-    return pop();
+    printf("\n");
+}
+
+void convertToHex(int num)
+{
+    while (num)
+    {
+        push(num%16);
+        num /= 16;
+    }
+}
+
+void displayHex()
+{
+    printf("\nHex : ");
+    while(top != -1)
+    {
+        int disp = pop();
+        switch (disp)
+        {
+        case 10:
+            printf("A");
+            break;
+        case 11:
+            printf("B");
+            break;
+        case 12:
+            printf("C");
+            break;
+        case 13:
+            printf("D");
+            break;
+        case 14:
+            printf("E");
+            break;
+        case 15:
+            printf("F");
+            break;
+        
+        default:
+            printf("%d",disp);
+            break;
+        }
+        
+    }
+    printf("\n\n");
 }
 
 int main()
 {
-    char exp[MAX]="23+";
-    printf("Enter a valid postfix expression: ");
-    scanf("%s",exp);
-    int result = evalPostfix(exp);
-    printf("Result : %d \n",result);
-
+    int num;
+    printf("Enter a Number: ");
+    scanf("%d",&num);
+    convertToBinary(num);
+    displayBinary();
+    
+    convertToHex(num);
+    displayHex();
     return 0;
 }
