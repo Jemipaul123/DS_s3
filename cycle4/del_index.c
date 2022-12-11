562b871ea5bb81ee8fd599cec474397ab34aa8d1
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2022-12-11 09:12:33 +0530

    Create del_index.c

---
+++ cycle4/del_index.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node*next;
+};
+struct node*head=NULL;
+
+void Createnode(int data){
+   struct node* newnode = (struct node*)malloc((sizeof(struct node)));
+    newnode->data=data;
+    newnode->next=NULL;
+
+if(head==NULL)
+{
+    head=newnode;
+}
+else{
+struct node* temp=head;
+while(temp->next !=NULL)
+{
+    temp=temp->next;
+}
+temp->next=newnode;}}
+void Atany(int data,int pos){
+   struct node* newnode = (struct node*)malloc((sizeof(struct node)));
+    newnode->data=data;
+    newnode->next=NULL;
+
+if(pos==0)
+{
+    newnode->next=head;
+     head=newnode;
+}
+
+else{
+struct node* temp=head;
+for(int i=1;i<pos;i++)
+{
+    temp=temp->next;
+}
+newnode->next=temp->next;
+temp->next=newnode;
+
+}}
+void Addfirst(int data)
+{
+   struct node* newnode = (struct node*)malloc((sizeof(struct node)));
+    newnode->data=data;
+    newnode->next=NULL;
+
+if(head==NULL)
+{
+    head=newnode;
+}
+else{
+newnode->next=head;
+head=newnode;
+
+}}
+void display(){
+    if(head==NULL)
+    printf("The list is empty\n");
+    else
+    {
+        struct node*temp=head;
+        while(temp!=NULL)
+        {
+            printf(" %d",temp->data);
+            temp=temp->next;
+        }
+        printf("\n");
+    }}
+    int main()
+{
+    int choice;
+    do
+    {
+        int data;int pos;
+        printf("1: Add element at end\n");
+        printf("2: Add element at first\n");
+        printf("3: Add element at any position\n");
+        printf("4: Delete element\n");
+        printf("5: Display the elements\n");
+        printf("6: Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d",&data);
+                Createnode(data);
+                break;
+            case 2:
+                printf("Enter value: ");
+                scanf("%d",&data);
+                Addfirst(data);
+                break;
+            case 3:
+                printf("Enter value: ");
+                scanf("%d",&data);
+                printf("enter pos");
+                scanf("%d",&pos);
+                Atany(data,pos);
+                break;
+            case 4:
+            printf("Enter the index to be deleted\n");
+            scanf("%d",&pos);
+            struct node* temp=head;
+            if (head==NULL)
+            printf(" ");
+            else if(pos==0)
+           { temp=temp->next;
+            head=temp;}
+            else{
+            for(int i=0;i<pos-1;i++)
+            {temp=temp->next;}
+            temp->next=temp->next->next;
+            printf("The element is deleted\n");}
+            break;
+            case 5:
+                display();
+                break;
+        
+            case 6:
+                break;
+            default:
+                printf("Invalid Choice\n");
+                break;
+        };
+    }while(choice != 6);
+    return 0;
+}   
